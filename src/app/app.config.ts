cab10f4d9df28eaed8ba73edcb07f8c28c06b1b0
SimonStorlSchulke <simonstoschu@googlemail.com>
2024-07-25 21:34:25 +0200

    use correct hashlocationstrategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { GalleryModule } from 'ng-gallery';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideRouter(routes),
     provideAnimations(),
     importProvidersFrom(GalleryModule),
