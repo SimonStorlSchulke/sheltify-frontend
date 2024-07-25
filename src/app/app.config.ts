4bf6a9913ab49391670cdfb0f1805c65c1ca27b1
SimonStorlSchulke <simonstoschu@googlemail.com>
2024-07-25 21:50:08 +0200

    fix routes?

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { GalleryModule } from 'ng-gallery';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withHashLocation()),
+    {provide: LocationStrategy, useClass: PathLocationStrategy},
     provideAnimations(),
     importProvidersFrom(GalleryModule),
     provideHttpClient()]
