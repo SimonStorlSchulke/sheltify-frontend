a4c7228446e48ee289f55a62328f2ef7f4a811af
SimonStorlSchulke <simonstoschu@googlemail.com>
2024-07-31 17:12:11 +0200

    fix deploy script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,5 +1,5 @@
 npm run build-prod
 sudo rm -rf /var/www/dev.herzenshunde-griechenland.de
 sudo mkdir /var/www/dev.herzenshunde-griechenland.de
-sudo cp -r ./doc/browser/* /var/www/dev.herzenshunde-griechenland.de
+sudo cp -r ./docs/browser/* /var/www/dev.herzenshunde-griechenland.de
 sudo cp ./.htaccess /var/www/dev.herzenshunde-griechenland.de
