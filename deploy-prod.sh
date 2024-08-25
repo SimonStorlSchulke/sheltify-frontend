7b03f488852beedbfaaf3de00390bcb793037821
SimonStorlSchulke <simonstoschu@googlemail.com>
2024-08-25 09:07:19 +0200

    fix prod deploy

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,7 +1,7 @@
 git reset --hard
 git pull
 npm run build-prod
-sudo rm -rf /var/www/herzenshunde-griechenland.de
-sudo mkdir /var/www/herzenshunde-griechenland.de
-sudo cp -r ./dist/browser/* /var/www/herzenshunde-griechenland.de
-sudo cp ./.htaccess /var/www/herzenshunde-griechenland.de
+sudo rm -rf /var/www/html
+sudo mkdir /var/www/html
+sudo cp -r ./dist/browser/* /var/www/html
+sudo cp ./.htaccess /var/www/html
